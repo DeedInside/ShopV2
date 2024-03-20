755cfb6762641fc0b9d1695090f59d3df9dee744
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-03-20 14:28:53 +0700

    v1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllersWithViews();
 
 // получаем строку подключения из файла конфигурации
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-
+ 
 // добавляем контекст ApplicationContext в качестве сервиса в приложение
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
